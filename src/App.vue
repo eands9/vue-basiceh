9f6c718808463afbbc48b0c145713123e0f9181f
eands9 <eands9@yahoo.com>
2022-07-19 20:45:39 -0500

    added xhr and sendmail api

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,30 @@ export default {
   async mounted() {
     const { text } = await (await fetch("/api/message")).json();
     this.message = text;
+
+    let formData = {
+      name: 'Eric',
+      email: 'eands9@yahoo.com',
+      subject: 'This is the Subject Line',
+      message: 'This is the message line!!!'
+    }
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/api/sendmail');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function(){
+        if(xhr.responseText == 'sucess'){
+            alert('Email sent');
+            name.value = '';
+            email.value = '';
+            subject.value = '';
+            message.value = '';
+        }else{
+            alert('Something went wrong!')
+        }
+    }
+
+    xhr.send(JSON.stringify(formData));
   }
 };
 </script>
